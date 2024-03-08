ff1f3927316f810dff3c45c8c3e54113e0a070bd
Jon Phipps <jphipps@madcreek.com>
2024-03-08 15:56:09 -0500

    add a custom valet environment file

--- .valet-env.php
+++ .valet-env.php
@@ -1,2 +1,10 @@
 <?php
 /** Created by PhpStorm,  User: jonphipps,  Date: 2024-03-08,  Time: 2:17 p.m. */
+
+return [
+    // Set $_SERVER['key'] to "value" for the omr24.test site...
+    'omr24' => [
+        'SERVER_NAME' => 'omr24.test',
+    ],
+    
+];
