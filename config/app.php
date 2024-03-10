79952e00e04db13c2b59887071ee31cd08aef8bd
Jon Phipps <jphipps@madcreek.com>
2024-03-10 12:01:50 -0400

    added telescope

--- config/app.php
+++ config/app.php
@@ -171,6 +171,7 @@ return [
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\Filament\AppPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
         App\Providers\SocialstreamServiceProvider::class,
